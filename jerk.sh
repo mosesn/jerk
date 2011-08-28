4b97efe063e6afef9276c69a1c5271e2a023ae27
Moses <nnythm@gmail.com>
2011-08-28 03:36:19 -0700

    quick revert

--- jerk.sh
+++ jerk.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+echo $1
+echo $2
+echo $3
 if [ -z "$1" ]; then
     echo What?
 else
     if [ "$1" = "save" ]; then
 	if [ -z "$2" ]; then
-	    echo Got a message for me?
+	    echo Save what?
 	else
 	    if [ -z "$3" ]; then
-		git commit -am "$2"
+		echo Got a message for me?
 	    else
-		if [ $2 = "all" ]; then
-		    git add -A
-		else
-		    git add $2
-		fi
+		git add $2
 		git commit -m "$3"
+		echo Fine whatever.
 	    fi
-	    echo Fine whatever.
 	fi
     elif [ "$1" = "ignore" ]; then
 	if [ -z "$2" ]; then
